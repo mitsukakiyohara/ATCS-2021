33443dae52c27a21f7e7803aa20c21940b835c14
Mitsuka Kiyohara <mitsuka@menlohacks.com>
2021-10-05 14:47:09 -0700

    if statement exercises

---
+++ project0/threeCrowd.py
@@ -0,0 +1,11 @@
+names = ["grace", "alex", "charlotte", "jhila", "marvin"]
+
+while(len(names) > 3): 
+    print("the room is crowded")
+    names.pop()
+else: 
+    print("the room is not very crowded")
+
+
+
+
